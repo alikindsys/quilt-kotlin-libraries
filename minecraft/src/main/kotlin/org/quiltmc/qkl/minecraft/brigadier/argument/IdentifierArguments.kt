a468db7c54244134a7bfeff34729113641d74736
alikindsys <alice@blocovermelho.org>
2024-12-22 15:00:26 -0300

    Migrate brigadier arguments
    Of note is that `EnumArguments.kt` required QSL so it couldn't be migrated to this project since I'm adhering to a strict import policy where only net.minecraft is allowed.

---
+++ minecraft/src/main/kotlin/org/quiltmc/qkl/minecraft/brigadier/argument/IdentifierArguments.kt
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2024 The Quilt Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Preserve binary compatibility when moving extensions between files
+ */
+@file:JvmMultifileClass
+@file:JvmName("ArgumentsKt")
+
+package org.quiltmc.qkl.minecraft.brigadier.argument
+
+
+import net.minecraft.advancement.AdvancementHolder
+import net.minecraft.command.argument.IdentifierArgumentType
+import net.minecraft.loot.condition.LootCondition
+import net.minecraft.loot.function.LootFunction
+import net.minecraft.recipe.RecipeHolder
+import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.unmapped.C_jjjbxeeq
+import net.minecraft.util.Identifier
+import org.quiltmc.qkl.minecraft.brigadier.*
+
+/**
+ * Reads the [Identifier] value from the
+ * argument in the receiver [ArgumentReader].
+ *
+ * @see IdentifierArgumentType.getIdentifier
+ *
+ * @author Cypher121
+ */
+@JvmName("valueIdentifierArg")
+@BrigadierDsl
+public fun DefaultArgumentReader<IdentifierArgumentType>.value(): Identifier {
+    return IdentifierArgumentType.getIdentifier(context.assumeSourceNotUsed(), name)
+}
+
+/**
+ * Reads the [Identifier] value from the
+ * argument in the receiver [ArgumentReader]
+ * as an [AdvancementHolder].
+ *
+ * @see IdentifierArgumentType.getAdvancementArgument
+ *
+ * @author Cypher121
+ */
+@BrigadierDsl
+public fun ArgumentReader<
+        ServerCommandSource,
+        DefaultArgumentDescriptor<
+                IdentifierArgumentType
+                >
+        >.asAdvancement(): AdvancementHolder {
+    return IdentifierArgumentType.getAdvancementArgument(context, name)
+}
+
+/**
+ * Reads the [Identifier] value from the
+ * argument in the receiver [ArgumentReader]
+ * as a [LootCondition].
+ *
+ * @see IdentifierArgumentType.getPredicateArgument
+ *
+ * @author Cypher121
+ */
+@BrigadierDsl
+public fun ArgumentReader<
+        ServerCommandSource,
+        DefaultArgumentDescriptor<
+                IdentifierArgumentType
+                >
+        >.asPredicate(): LootCondition {
+    return C_jjjbxeeq.method_58488(context, name).value()
+}
+
+/**
+ * Reads the [Identifier] value from the
+ * argument in the receiver [ArgumentReader]
+ * as a [LootFunction].
+ *
+ * @see IdentifierArgumentType.getItemModifierArgument
+ *
+ * @author Cypher121
+ */
+@BrigadierDsl
+public fun ArgumentReader<
+        ServerCommandSource,
+        DefaultArgumentDescriptor<
+                IdentifierArgumentType
+                >
+        >.asItemModifier(): LootFunction {
+    return C_jjjbxeeq.method_58485(context, name).value()
+}
+
+/**
+ * Reads the [Identifier] value from the
+ * argument in the receiver [ArgumentReader]
+ * as a [RecipeHolder].
+ *
+ * @see IdentifierArgumentType.getRecipeArgument
+ *
+ * @author Cypher121
+ */
+@BrigadierDsl
+public fun ArgumentReader<
+        ServerCommandSource,
+        DefaultArgumentDescriptor<
+                IdentifierArgumentType
+                >
+        >.asRecipe(): RecipeHolder<*> {
+    return IdentifierArgumentType.getRecipeArgument(context, name)
+}
+
+/**
+ * Creates an identifier argument with [name] as the parameter name.
+ *
+ * @author Oliver-makes-code (Emma)
+ */
+@BrigadierDsl
+public fun <S> identifier(
+    name: String
+): DefaultArgumentConstructor<S, IdentifierArgumentType> {
+    return argument(name, IdentifierArgumentType.identifier())
+}
---
+++ minecraft/src/main/kotlin/org/quiltmc/qkl/minecraft/brigadier/argument/TextArguments.kt
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2023 The Quilt Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Preserve binary compatibility when moving extensions between files
+ */
+@file:JvmMultifileClass
+@file:JvmName("ArgumentsKt")
+
+package org.quiltmc.qkl.minecraft.brigadier.argument
+
+
+import net.minecraft.command.CommandBuildContext
+import net.minecraft.command.argument.ColorArgumentType
+import net.minecraft.command.argument.MessageArgumentType
+import net.minecraft.command.argument.TextArgumentType
+import net.minecraft.network.message.SignedChatMessage
+import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
+import org.quiltmc.qkl.minecraft.brigadier.*
+
+
+/**
+ * Reads the message's [Text] value from
+ * the argument in the receiver [ArgumentReader].
+ *
+ * @see MessageArgumentType.getMessage
+ *
+ * @author Cypher121
+ */
+@JvmName("valueMessageArg")
+@BrigadierDsl
+public fun ArgumentReader<
+        ServerCommandSource,
+        DefaultArgumentDescriptor<
+                MessageArgumentType
+                >
+        >.value(): Text {
+    return MessageArgumentType.getMessage(context, name)
+}
+
+/**
+ * Reads the [SignedChatMessage] value from
+ * the argument in the receiver [ArgumentReader]
+ * and calls [consumer] with it as an argument.
+ *
+ * The value may not be available immediately.
+ *
+ * @see MessageArgumentType.resolveSignedMessage
+ *
+ * @author Cypher121
+ */
+@BrigadierDsl
+public fun ArgumentReader<
+        ServerCommandSource,
+        DefaultArgumentDescriptor<
+                MessageArgumentType
+                >
+        >.resolveSigned(consumer: (SignedChatMessage) -> Unit) {
+    MessageArgumentType.resolveChatMessage(context, name, consumer)
+}
+
+/**
+ * Reads the selected color's [Formatting] value from
+ * the argument in the receiver [ArgumentReader].
+ *
+ * @see ColorArgumentType.getColor
+ *
+ * @author Cypher121
+ */
+@JvmName("valueColorArg")
+@BrigadierDsl
+public fun DefaultArgumentReader<ColorArgumentType>.value(): Formatting {
+    return ColorArgumentType.getColor(context.assumeSourceNotUsed(), name)
+}
+
+/**
+ * Reads the [Text] value from the
+ * argument in the receiver [ArgumentReader].
+ *
+ * @see TextArgumentType.getTextArgument
+ *
+ * @author Cypher121
+ */
+@JvmName("valueTextArg")
+@BrigadierDsl
+public fun DefaultArgumentReader<TextArgumentType>.value(): Text {
+    return TextArgumentType.getTextArgument(context.assumeSourceNotUsed(), name)
+}
+
+/**
+ * Creates a message argument with [name] as the parameter name.
+ *
+ * @author Oliver-makes-code (Emma)
+ */
+@BrigadierDsl
+public fun <S> message(
+    name: String
+): DefaultArgumentConstructor<S, MessageArgumentType> {
+    return argument(name, MessageArgumentType.message())
+}
+
+/**
+ * Creates a color argument with [name] as the parameter name.
+ *
+ * @author Oliver-makes-code (Emma)
+ */
+@BrigadierDsl
+public fun <S> color(
+    name: String
+): DefaultArgumentConstructor<S, ColorArgumentType> {
+    return argument(name, ColorArgumentType.color())
+}
+
+/**
+ * Creates a text argument with [name] as the parameter name.
+ *
+ * @author Oliver-makes-code (Emma)
+ */
+@BrigadierDsl
+public fun <S> text(
+    context: CommandBuildContext,
+    name: String
+): DefaultArgumentConstructor<S, TextArgumentType> {
+    return argument(name, TextArgumentType.text(context))
+}
